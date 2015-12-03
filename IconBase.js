6d8d7a18e60720902fda1f01b8e32baa4092612f
Goran Gajic <fero.null@gmail.com>
2015-12-04 00:44:41 +0100

    use react-icon-base

--- IconBase.js
+++ IconBase.js
@@ -1,35 +1,3 @@
-import React from 'react';
-
-class IconBase extends React.Component {
-    render() {
-        let styles = {
-            verticalAlign: "middle",
-        };
-        var props = {
-            fill: "currentColor",
-            width: this.props.size,
-            height: this.props.size
-        }
-        return (
-            <svg {...props} {...this.props}
-                 preserveAspectRatio="xMidYMid meet" fit
-                 style={{...styles, ...this.props.style}} >
-                {this.props.children}
-            </svg>
-        );
-    }
-};
-
-IconBase.defaultProps = {
-    size: '1em'
-};
-
-IconBase.propTypes = {
-    size: React.PropTypes.oneOfType([
-        React.PropTypes.string,
-        React.PropTypes.number
-    ]),
-    style: React.PropTypes.object
-};
+import IconBase from 'react-icon-base';
 
 export default IconBase;

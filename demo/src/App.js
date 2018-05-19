2827cde6d1e527583d50c984ff898a18721a67b0
kamijin_fanta <kamijin@live.jp>
2018-05-19 14:53:24 +0900

    demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,6 +2,28 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+export function GenIcon(data) {
+  return props => (
+    <IconBase viewBox={data.viewBox} {...props}>
+      {data.path.map((path, index) => <path key={index} {...path} />)}
+    </IconBase>
+  );
+}
+export function IconBase(props) {
+  const computedSize = props.size || "1em";
+  return (
+    <svg
+      style={props.style}
+      viewBox={props.viewBox}
+      height={computedSize}
+      width={computedSize}
+    >
+      {props.children}
+    </svg>
+  );
+}
+const MdAccessibility = GenIcon(Icon.MdAccessibility);
+
 class App extends Component {
   render() {
     return (
@@ -9,6 +31,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <MdAccessibility />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.

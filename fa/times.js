f432454f7265f4b1e9f0adcef28078bf1fc69dc8
Peter Shershov <peter.shershov@kenshoo.com>
2018-06-20 11:14:44 +0300

    master(fa-times): fa-times icon added to Fa directory

---
+++ fa/times.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import Icon from 'react-icon-base'
+
+const FaTimes = props => (
+    <Icon viewBox="0 0 40 40" {...props}>
+        <g><path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"/></g>
+    </Icon>
+);
+
+export default FaTimes;

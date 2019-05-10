109e8d7c4b3ab00ff4749cf3e86ab95454eb231a
kamijin_fanta <kamijin@live.jp>
2019-05-10 12:52:47 +0900

    fix ts support / add test for ts project

---
+++ packages/ts-test/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import {IconContext} from 'react-icons'
+import {FaBeer} from 'react-icons/fa'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <IconContext.Provider value={{color: '#fa7'}}>
+          <FaBeer />
+        </IconContext.Provider>
+      </header>
+    </div>
+  );
+}
+
+export default App;

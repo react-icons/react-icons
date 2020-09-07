6be8d5395fb460169a05bb1d5f903d88ae6776d0
kamijin_fanta <kamijin@live.jp>
2020-09-07 11:45:45 +0900

    starting change project struct.
    - split build processes
    - output number of icons to VERSIONS
    - PoC that outputs has many files packages

---
+++ packages/react-icons/scripts/logics.js
@@ -0,0 +1,69 @@
+const cheerio = require("cheerio");
+const glob = require("glob-promise");
+const camelcase = require("camelcase");
+
+async function getIconFiles(content) {
+  return glob(content.files);
+}
+async function convertIconData(svg, multiColor) {
+  const $svg = cheerio.load(svg, { xmlMode: true })("svg");
+
+  // filter/convert attributes
+  // 1. remove class attr
+  // 2. convert to camelcase ex: fill-opacity => fillOpacity
+  const attrConverter = (
+    /** @type {{[key: string]: string}} */ attribs,
+    /** @type string */ tagName
+  ) =>
+    attribs &&
+    Object.keys(attribs)
+      .filter(
+        name =>
+          ![
+            "class",
+            ...(tagName === "svg"
+              ? ["xmlns", "xmlns:xlink", "xml:space", "width", "height"]
+              : []) // if tagName is svg remove size attributes
+          ].includes(name)
+      )
+      .reduce((obj, name) => {
+        const newName = camelcase(name);
+        switch (newName) {
+          case "fill":
+            if (
+              attribs[name] === "none" ||
+              attribs[name] === "currentColor" ||
+              multiColor
+            ) {
+              obj[newName] = attribs[name];
+            }
+            break;
+          default:
+            obj[newName] = attribs[name];
+            break;
+        }
+        return obj;
+      }, {});
+
+  // convert to [ { tag: 'path', attr: { d: 'M436 160c6.6 ...', ... }, child: { ... } } ]
+  const elementToTree = (/** @type {Cheerio} */ element) =>
+    element
+      .filter((_, e) => e.tagName && !["style"].includes(e.tagName))
+      .map((_, e) => ({
+        tag: e.tagName,
+        attr: attrConverter(e.attribs, e.tagName),
+        child:
+          e.children && e.children.length
+            ? elementToTree(cheerio(e.children))
+            : undefined
+      }))
+      .get();
+
+  const tree = elementToTree($svg);
+  return tree[0]; // like: [ { tag: 'path', attr: { d: 'M436 160c6.6 ...', ... }, child: { ... } } ]
+}
+
+module.exports = {
+  getIconFiles,
+  convertIconData
+};

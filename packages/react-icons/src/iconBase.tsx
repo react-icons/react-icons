95c49d431dc28ff47ab00183ca513d2fb27266d1
Jess Telford <jess+github@jes.st>
2020-09-16 17:19:01 +1000

    Forward refs to the underlaying svg components

--- packages/react-icons/src/iconBase.tsx
+++ packages/react-icons/src/iconBase.tsx
@@ -8,15 +8,16 @@ export interface IconTree {
   child: IconTree[];
 }
 
-
 function Tree2Element(tree: IconTree[]): React.ReactElement<{}>[] {
   return tree && tree.map((node, i) => React.createElement(node.tag, {key: i, ...node.attr}, Tree2Element(node.child)));
 }
 export function GenIcon(data: IconTree) {
-  return (props: IconBaseProps) => (
-    <IconBase attr={{...data.attr}} {...props}>
-      {Tree2Element(data.child)}
-    </IconBase>
+  return React.forwardRef<JSX.Element, IconBaseProps>(
+    (props: IconBaseProps, ref) => (
+      <IconBase ref={ref} attr={{...data.attr}} {...props}>
+        {Tree2Element(data.child)}
+      </IconBase>
+    )
   );
 }
 
@@ -27,36 +28,44 @@ export interface IconBaseProps extends React.SVGAttributes<SVGElement> {
   title?: string;
 }
 
-export type IconType = (props: IconBaseProps) => JSX.Element;
-export function IconBase(props:IconBaseProps & { attr?: {} }): JSX.Element {
-  const elem = (conf: IconContext) => {
-    const computedSize = props.size || conf.size || "1em";
-    let className;
-    if (conf.className) className = conf.className;
-    if (props.className) className = (className ? className + ' ' : '') + props.className;
-    const {attr, title, ...svgProps} = props;
-
-    return (
-      <svg
-        stroke="currentColor"
-        fill="currentColor"
-        strokeWidth="0"
-        {...conf.attr}
-        {...attr}
-        {...svgProps}
-        className={className}
-        style={{ color: props.color || conf.color, ...conf.style, ...props.style}}
-        height={computedSize}
-        width={computedSize}
-        xmlns="http://www.w3.org/2000/svg"
-      >
-      {title && <title>{title}</title>}
-      {props.children}
-    </svg>
-    )
-  };
-
-  return IconContext !== undefined
-    ? <IconContext.Consumer>{(conf: IconContext) => elem(conf)}</IconContext.Consumer>
-    : elem(DefaultContext);
+interface IconBasePropsWithAttr extends IconBaseProps {
+  attr?: {};
 }
+
+export type IconType = (props: IconBaseProps) => JSX.Element;
+
+export const IconBase = React.forwardRef<any, IconBasePropsWithAttr>(
+  (props: IconBasePropsWithAttr, ref): JSX.Element => {
+    const elem = (conf: IconContext) => {
+      const computedSize = props.size || conf.size || "1em";
+      let className;
+      if (conf.className) className = conf.className;
+      if (props.className) className = (className ? className + ' ' : '') + props.className;
+      const {attr, title, ...svgProps} = props;
+
+      return (
+        <svg
+          ref={ref}
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          {...conf.attr}
+          {...attr}
+          {...svgProps}
+          className={className}
+          style={{ color: props.color || conf.color, ...conf.style, ...props.style}}
+          height={computedSize}
+          width={computedSize}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+        {title && <title>{title}</title>}
+        {props.children}
+      </svg>
+      )
+    };
+
+    return IconContext !== undefined
+      ? <IconContext.Consumer>{(conf: IconContext) => elem(conf)}</IconContext.Consumer>
+      : elem(DefaultContext);
+  }
+);

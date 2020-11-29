b32df18412389ffe8acb4e54ae1ce2822ccf7116
Gabriel Gavilan <ggavilan@redpoints.com>
2020-11-29 13:28:42 +0100

    Add highlighting in search.

--- packages/preview/src/components/@core/icon/index.tsx
+++ packages/preview/src/components/@core/icon/index.tsx
@@ -2,7 +2,7 @@ import toast from "cogo-toast";
 import copy from "copy-to-clipboard";
 import React from "react";
 
-function Icon({ icon, name }) {
+function Icon({ icon, name, highlightPattern = null }) {
   const copyToClipboard = () => {
     copy(name);
     toast.success(`Copied '${name}' to clipboard`, {
@@ -10,10 +10,18 @@ function Icon({ icon, name }) {
     });
   };
 
+  const highlightedName = () => {
+    if (highlightPattern)
+      return name
+        .split(highlightPattern)
+        .map((part) => (part.match(highlightPattern) ? <b>{part}</b> : part));
+    return name;
+  };
+
   return (
     <div className="item" tabIndex={0} onClick={copyToClipboard} key={name}>
       <div className="icon h2">{typeof icon === "function" && icon()}</div>
-      <div className="name">{name}</div>
+      <div className="name">{highlightedName()}</div>
     </div>
   );
 }
--- packages/preview/src/components/pages/search/index.tsx
+++ packages/preview/src/components/pages/search/index.tsx
@@ -15,26 +15,27 @@ export default function SearchPageComponent() {
       0
   }
 
-  return query.length > 2 ? (
-    <>
-      <h2>
-        Results for: <i>{query}</i>
-      </h2>
-      <div className="icons">
-        {allIcons.map(icon => (
-          <SearchIconSet
-            key={icon.id}
-            icon={icon}
-            query={query}
-            setResults={setResults} />
-        ))}
-      </div>
-      { 
-        getTotal(results) === 0 &&
-        <h3>No icons found</h3>
-      }
-    </>
-  ) : (
-    <h2>Please enter at least 3 characters to search...</h2>
-  );
+  if (query.length > 2) {
+    const hightlightPattern = new RegExp(`(${query})`, "i");
+    return (
+      <>
+        <h2>
+          Results for: <i>{query}</i>
+        </h2>
+        <div className="icons">
+          {allIcons.map((icon) => (
+            <SearchIconSet
+              key={icon.id}
+              icon={icon}
+              query={query}
+              setResults={setResults}
+              highlightPattern={hightlightPattern}
+            />
+          ))}
+        </div>
+        {getTotal(results) === 0 && <h3>No icons found</h3>}
+      </>
+    );
+  }
+  return <h2>Please enter at least 3 characters to search...</h2>;
 }

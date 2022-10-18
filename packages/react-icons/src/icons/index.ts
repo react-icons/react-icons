aefa9cd92a720719df2da69c976012cc28b07f3b
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2022-10-18 22:11:58 +0200

    Update heroicons 2 to v2.0.12

--- packages/react-icons/src/icons/index.ts
+++ packages/react-icons/src/icons/index.ts
@@ -432,6 +432,13 @@ export const icons: IconDefinition[] = [
         ),
         formatter: (name) => `HiOutline${name}`,
       },
+      {
+        files: path.resolve(
+          __dirname,
+          "heroicons-2/optimized/20/solid/*.svg"
+        ),
+        formatter: (name) => `HiMini${name}`,
+      },
     ],
     projectUrl: "https://github.com/tailwindlabs/heroicons",
     license: "MIT",

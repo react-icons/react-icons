83036cff8ff9e1904d091157e30dca064e402d86
kamijin_fanta <kamijin@live.jp>
2022-10-22 13:39:27 +0900

    build-script.sh: fetch icon sets

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eux
 
+(cd packages/react-icons/ && yarn fetch)
 (cd packages/react-icons/ && yarn build)
 echo VERSIONS; cat packages/react-icons/VERSIONS
 (cd packages/_react-icons_all/ && npm pack |& tail)

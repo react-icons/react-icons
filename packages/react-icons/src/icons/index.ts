08a27a0806458be2b406436bb4fd9ecaa500dc18
John William Kotronakis Karras <64101252+gitChaoslord@users.noreply.github.com>
2023-06-01 17:55:27 +0300

    Feature: additional variants for BoxIcons (Solid/Logo) (#724)
    * feat: added additional variants for BoxIcons (Solid/Logo)
    
    * format
    
    ---------
    
    Co-authored-by: kamijin_fanta <kamijin@live.jp>

--- packages/react-icons/src/icons/index.ts
+++ packages/react-icons/src/icons/index.ts
@@ -624,6 +624,14 @@ export const icons: IconDefinition[] = [
         ),
         formatter: (name) => `Bi${name.replace("Bx", "")}`,
       },
+      {
+        files: path.resolve(__dirname, "../../icons/boxicons/svg/solid/*.svg"),
+        formatter: (name) => `BiSolid${name.replace("Bxs", "")}`,
+      },
+      {
+        files: path.resolve(__dirname, "../../icons/boxicons/svg/logos/*.svg"),
+        formatter: (name) => `BiLogo${name.replace("Bxl", "")}`,
+      },
     ],
     projectUrl: "https://github.com/atisawd/boxicons",
     license: "CC BY 4.0 License",
@@ -631,7 +639,7 @@ export const icons: IconDefinition[] = [
     source: {
       type: "git",
       localName: "boxicons",
-      remoteDir: "svg/regular/",
+      remoteDir: "svg/",
       url: "https://github.com/atisawd/boxicons.git",
       branch: "master",
       hash: "9ffa9136e8681886bb7bd2145cd4098717ce1c11",

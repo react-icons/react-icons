d92162f68f977ea8055be365aa762a3bf8aecd8f
kamijin_fanta <kamijin@live.jp>
2023-11-14 13:12:48 +0900

    fix build script for astro preview site

--- build-script.sh
+++ build-script.sh
@@ -6,7 +6,7 @@ echo VERSIONS; cat packages/react-icons/VERSIONS
 (cd packages/_react-icons_all/ && npm pack |& tail)
 (cd packages/_react-icons_all-files/ && npm pack |& tail)
 
-(cd packages/preview/ && yarn build)
+(cd packages/preview-astro/ && yarn build)
 (cd packages/demo/ && CI=true yarn test && yarn build)
 (cd packages/webpack4-test/ && CI=true yarn test)
 (cd packages/ts-test/ && SKIP_PREFLIGHT_CHECK=true yarn build)

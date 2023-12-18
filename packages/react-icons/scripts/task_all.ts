9a6a817054f9ef3a7dfe75dfd61eae945f9e31df
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-12-19 00:30:53 +0200

    Convert build pipeline to produce valid ESM output

--- packages/react-icons/scripts/task_all.ts
+++ packages/react-icons/scripts/task_all.ts
@@ -16,13 +16,11 @@ export async function dirInit({ DIST, LIB, rootDir }) {
   await rmDirRecursive(DIST);
   await fs.mkdir(DIST, { recursive: true }).catch(ignore);
   await fs.mkdir(LIB).catch(ignore);
-  await fs.mkdir(path.resolve(LIB, "esm")).catch(ignore);
-  await fs.mkdir(path.resolve(LIB, "cjs")).catch(ignore);
 
   const write = (filePath, str) =>
     fs.writeFile(path.resolve(DIST, ...filePath), str, "utf8").catch(ignore);
 
-  const initFiles = ["index.d.ts", "index.esm.js", "index.js"];
+  const initFiles = ["index.d.ts", "index.mjs", "index.js"];
 
   for (const icon of icons) {
     await fs.mkdir(path.resolve(DIST, icon.id)).catch(ignore);
@@ -32,19 +30,19 @@ export async function dirInit({ DIST, LIB, rootDir }) {
       "// THIS FILE IS AUTO GENERATED\nvar GenIcon = require('../lib').GenIcon\n",
     );
     await write(
-      [icon.id, "index.esm.js"],
-      "// THIS FILE IS AUTO GENERATED\nimport { GenIcon } from '../lib';\n",
+      [icon.id, "index.mjs"],
+      "// THIS FILE IS AUTO GENERATED\nimport { GenIcon } from '../lib/index.mjs';\n",
     );
     await write(
       [icon.id, "index.d.ts"],
-      "// THIS FILE IS AUTO GENERATED\nimport { IconTree, IconType } from '../lib'\n",
+      "// THIS FILE IS AUTO GENERATED\nimport { IconTree, IconType } from '../lib/index.mjs'\n",
     );
     await write(
       [icon.id, "package.json"],
       JSON.stringify(
         {
           sideEffects: false,
-          module: "./index.esm.js",
+          module: "./index.mjs",
         },
         null,
         2,
@@ -78,10 +76,10 @@ export async function writeIconModule(icon, { DIST, LIB, rootDir }) {
       if (exists.has(name)) continue;
       exists.add(name);
 
-      // write like: module/fa/index.esm.js
+      // write like: module/fa/index.mjs
       const modRes = iconRowTemplate(icon, name, iconData, "module");
       await fs.appendFile(
-        path.resolve(DIST, icon.id, "index.esm.js"),
+        path.resolve(DIST, icon.id, "index.mjs"),
         modRes,
         "utf8",
       );

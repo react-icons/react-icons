e2a3251090c678745ba9eb0ec8efa4515b0d261e
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-12-19 02:42:08 +0200

    Fix package root entry point exports

--- packages/react-icons/scripts/task_common.ts
+++ packages/react-icons/scripts/task_common.ts
@@ -58,9 +58,7 @@ export async function writeEntryPoints({ DIST, LIB, rootDir }) {
     return `module.exports = require('./lib/index.js');`;
   };
   const generateEntryMjs = function (filename = "index.mjs") {
-    return `import * as m from './lib/${filename}'
-export default m
-    `;
+    return `export * from './lib/${filename}';`;
   };
   await fs.appendFile(
     path.resolve(DIST, "index.js"),

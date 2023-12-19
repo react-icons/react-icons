dbd93b8b35e1fedc82b1f3239585a26a83f1f8a3
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-12-19 02:54:05 +0200

    Drop `demo-all-files` folder

--- packages/demo-all-files/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-
-createRoot(document.getElementById("root")).render(<App />);
-registerServiceWorker();

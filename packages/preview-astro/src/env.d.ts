b5f76f318d20a7f5080e2be3409f0297233be0fe
kamijin_fanta <kamijin@live.jp>
2024-04-30 18:45:47 +0900

    upgrade eslint 9.x

--- packages/preview-astro/src/env.d.ts
+++ packages/preview-astro/src/env.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../.astro/types.d.ts" />
+// /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
 declare module "virtual:react-icons-get-icons" {
--- packages/react-icons/src/iconBase.tsx
+++ packages/react-icons/src/iconBase.tsx
@@ -15,13 +15,12 @@ function Tree2Element(tree: IconTree[]): React.ReactElement[] {
       React.createElement(
         node.tag,
         { key: i, ...node.attr },
-        Tree2Element(node.child)
-      )
+        Tree2Element(node.child),
+      ),
     )
   );
 }
 export function GenIcon(data: IconTree) {
-  // eslint-disable-next-line react/display-name
   return (props: IconBaseProps) => (
     <IconBase attr={{ ...data.attr }} {...props}>
       {Tree2Element(data.child)}
@@ -38,7 +37,7 @@ export interface IconBaseProps extends React.SVGAttributes<SVGElement> {
 
 export type IconType = (props: IconBaseProps) => JSX.Element;
 export function IconBase(
-  props: IconBaseProps & { attr?: Record<string, string> }
+  props: IconBaseProps & { attr?: Record<string, string> },
 ): JSX.Element {
   const elem = (conf: IconContext) => {
     const { attr, size, title, ...svgProps } = props;

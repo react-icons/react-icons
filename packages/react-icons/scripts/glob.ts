829d3a70f18bd59a446fd39a60edf4d25578d27b
kamijin_fanta <kamijin@live.jp>
2024-04-30 18:58:55 +0900

    fix glob type

--- packages/react-icons/scripts/glob.ts
+++ packages/react-icons/scripts/glob.ts
@@ -1,4 +1,4 @@
-import { glob as rawGlob } from "glob-promise";
+import rawGlob from "glob-promise";
 export function glob(pattern: string): Promise<string[]> {
-  return rawGlob.glob(pattern.replace(/\\/g, "/")); // convert windows path
+  return rawGlob(pattern.replace(/\\/g, "/")); // convert windows path
 }

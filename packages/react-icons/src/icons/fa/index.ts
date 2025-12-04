2257de0add1e2538a3b78e7f195236145d12b203
PREM015 <152625984+PREM015@users.noreply.github.com>
2025-12-04 16:55:35 +0530

    Add FA Bow icon and update related files

---
+++ packages/react-icons/src/icons/fa/index.ts
@@ -0,0 +1,21 @@
+import path from "path";
+import { IconDefinition } from "../../scripts/_types";
+
+export const icons: IconDefinition[] = [
+  {
+    id: "fa",
+    name: "Font Awesome Custom",
+    projectUrl: "https://github.com/PREM015/react-icons",
+    license: "MIT",
+    licenseUrl: "https://opensource.org/licenses/MIT",
+    contents: [
+      {
+        files: path.resolve(__dirname, "./*.svg"),
+        formatter: (name: string) => `Fa${name}`,
+        processWithSVGO: true,
+      },
+    ],
+  },
+];
+
+export default icons;
